de5e85942cfbd5b18831adb35416515a83a700b5
Amit Singh <147734113+AMITSINGH1027@users.noreply.github.com>
2025-04-22 10:05:11 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,43 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {6, 5, 2, 3, 1};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        selectionSort(arr);
+        
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+       
+            int minIndex = i;
+
+         
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+           
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;                                              
+            System.out.println("After pass " + (i + 1) + ":");
+            printArray(arr);
+        }
+    }
+   
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
